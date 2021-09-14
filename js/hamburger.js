5ce293ccc4176ee6bcfbdbd1a3bd10bcf04b3d71
畑龍太郎 <ryutaro506@gmail.com>
2021-09-14 23:38:54 +0900

    index.html complete

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,8 @@
 $(function($) {
     $(".js-hamburger").on("click", function() {
         $(this).toggleClass("is-open");
-        $("p-gmenu").toggleClass("is-open");
-        $(".nav-contener").slideToggle(200);
+        $(".l-gmenu").toggleClass("is-open");
+        $("body").toggleClass("is-open");
+        console.log("test")
     });
 });
